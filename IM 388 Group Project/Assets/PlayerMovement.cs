52212bc3eaccccafbb97e3c0312ad787efafea46
jwelc <jwelch@mail.bradley.edu>
2021-08-31 10:40:05 -0500

    Improvements to player movement
    The camera now transitions more smoothly between cannons and has more look ahead time. Fixes to the cannon movement script

--- IM 388 Group Project/Assets/PlayerMovement.cs
+++ IM 388 Group Project/Assets/PlayerMovement.cs
@@ -6,13 +6,17 @@
 // Brief Description : Handles the movement of the player.
 *****************************************************************************/
 using UnityEngine;
+using Cinemachine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     #region Variables
+    #region Shoot Forces
+    [Header("Shoot forces")]
     [SerializeField]
     [Tooltip("How much force cannons shoot out objects")]
-    [Range(500, 5000)]
+    [Range(0, 5000)]
     private float shootForce = 1000;
 
     [SerializeField]
@@ -24,7 +28,10 @@ public class PlayerMovement : MonoBehaviour
     [Tooltip("How much angular momentum is added to the cannon when it is shot")]
     [Range(200, 4000)]
     private float shootTorque = 800;
+    #endregion
 
+    #region Speed Limits
+    [Header("Speed Limits")]
     [SerializeField]
     [Tooltip("The max velocity of cannons")]
     [Range(5, 30)]
@@ -34,55 +41,152 @@ public class PlayerMovement : MonoBehaviour
     [Tooltip("The max amount of spin a cannon can have")]
     [Range(300, 1000)]
     private float maxAngularVelocity = 600;
+    #endregion
 
+    #region Shoot Positions
+    [Header("Shoot positions")]
     [SerializeField]
     [Tooltip("The position the cannon is shot from")]
     private GameObject shootPos;
 
     [SerializeField]
-    [Tooltip("The cannon prefab that is left behind")]
-    private GameObject cannonPrefab;
+    [Tooltip("The position the cannon pivots from")]
+    private GameObject pivotPos;
+    #endregion
+
+    #region Extras
+    [Header("Extras")]
+    [SerializeField]
+    [Tooltip("The cannon part of the heirarchy")]
+    private GameObject cannon;
+    #endregion
+
+    #region Components
+    /// <summary>
+    /// Current Cinemachine being used.
+    /// </summary>
+    [HideInInspector]
+    public CinemachineVirtualCamera currentVCam;
 
     /// <summary>
     /// The Rigidbody2D component of this cannon.
     /// </summary>
-    private Rigidbody2D rb2d;
+    [HideInInspector]
+    public Rigidbody2D rb2d;
+
+    /// <summary>
+    /// Reference to the main camera in this scene.
+    /// </summary>
+    private Camera mainCam;
+    #endregion
+
+    #region Bools
+    /// <summary>
+    /// Holds true if the player is allowed to aim the cannon.
+    /// </summary>
+    private static bool canAim = true;
+
+    /// <summary>
+    /// Allows or disables the cannons ability to shoot.
+    /// </summary>
+    private bool canShoot = true;
+    #endregion
     #endregion
 
     #region Functions
+    /// <summary>
+    /// Initializes components.
+    /// </summary>
     private void Awake()
     {
+        currentVCam = transform.parent.gameObject.GetComponentInChildren<CinemachineVirtualCamera>();
+        mainCam = Camera.main;
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    /// <summary>
+    /// Resets the speed and angular velocity.
+    /// </summary>
+    private void FixedUpdate()
     {
-        if(rb2d.velocity.magnitude > maxSpeed)
+        LimitSpeed();
+        LimitAngularVelocity();
+    }
+
+    /// <summary>
+    /// Limits how fast the player can move.
+    /// </summary>
+    private void LimitSpeed()
+    {
+        if (rb2d.velocity.magnitude > maxSpeed)
         {
             Vector2 newVel = rb2d.velocity.normalized * maxSpeed;
             rb2d.velocity = newVel;
         }
+    }
 
-        if(rb2d.angularVelocity > maxAngularVelocity)
+    /// <summary>
+    /// Limits the amount of rotation on the player.
+    /// </summary>
+    private void LimitAngularVelocity()
+    {
+        if (rb2d.angularVelocity > maxAngularVelocity)
         {
             rb2d.angularVelocity = maxAngularVelocity;
         }
     }
 
+    /// <summary>
+    /// Aims the cannon.
+    /// </summary>
+    /// <param name="input">The mouse position.</param>
+    public void OnLook(InputValue input)
+    {
+        if (canAim)
+        {
+            // Calculates the angle the cannon should be
+            Vector2 inputVec = input.Get<Vector2>();
+            Vector3 aimDir = (mainCam.ScreenToWorldPoint(inputVec) - cannon.transform.position).normalized;
+            float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
+
+            cannon.transform.eulerAngles = new Vector3(0, 0, angle);
+        }
+    }
+
+    /// <summary>
+    /// If the shoot key is pressed then a new character is shot out.
+    /// </summary>
     public void OnShootPlayer()
     {
-        GameObject tempCannon = Instantiate(cannonPrefab, transform.position, Quaternion.identity);
+        if (canShoot)
+        {
+            // Disables cannons ability to shoot and aim
+            canShoot = false;
+            canAim = false;
 
-        tempCannon.transform.localScale = transform.localScale;
-        gameObject.transform.localScale = transform.localScale / 1.2f;
+            //Spawns cannon
+            GameObject tempCannon = Instantiate(Resources.Load("Prefabs/Cannon and Camera", typeof(GameObject)), (Vector2)shootPos.transform.position, Quaternion.identity) as GameObject;
+            tempCannon.transform.localScale = transform.parent.transform.localScale / 1.2f;
 
-        Vector2 shootDir = shootPos.transform.position - transform.position;
-        shootDir.Normalize();
-        tempCannon.GetComponent<Rigidbody2D>().AddForce(-shootDir * shootPushBackForce);
+            // Gets direction cannon should be shot
+            Vector2 shootDir = shootPos.transform.position - pivotPos.transform.position;
+            shootDir.Normalize();
 
-        transform.position = shootPos.transform.position;
-        rb2d.AddForce(shootDir * shootForce);
-        rb2d.AddTorque(shootTorque);
+            // Adds forces to both cannons and changes the current camera
+            rb2d.AddForce(-shootDir * shootPushBackForce);
+            PlayerMovement tempPM = tempCannon.GetComponentInChildren<PlayerMovement>();
+            tempPM.rb2d.velocity = shootDir * shootForce;
+            tempPM.rb2d.AddTorque(shootTorque);
+            tempPM.currentVCam.Priority = currentVCam.Priority + 1;
+        }
+    }
+
+    /// <summary>
+    /// Destroys objects once they go off of the screen.
+    /// </summary>
+    private void OnBecameInvisible()
+    {
+        Destroy(transform.parent.gameObject);
     }
     #endregion
 }
