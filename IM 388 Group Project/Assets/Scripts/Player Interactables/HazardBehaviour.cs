47e8061c25f6e62677e8a93b12cc24b27f2e454b
jwelc <jwelch@mail.bradley.edu>
2021-09-10 22:53:43 -0500

    Level Changes and Fire Ring Update

--- IM 388 Group Project/Assets/Scripts/Player Interactables/HazardBehaviour.cs
+++ IM 388 Group Project/Assets/Scripts/Player Interactables/HazardBehaviour.cs
@@ -10,28 +10,14 @@ using UnityEngine;
 public class HazardBehaviour : MonoBehaviour, IPlayerInteractable
 {
     /// <summary>
-    /// Holds true if the player has died.
-    /// </summary>
-    private static bool hasDied = false;
-
-    /// <summary>
     /// Handles the collision between the player and this hazard which restarts the level.
     /// </summary>
     /// <param name="other"></param>
     public void CollisionEvent(GameObject other)
     {
-        if(!FlagBehavior.HasWon && !hasDied)
+        if(!FlagBehavior.HasWon)
         {
             other.gameObject.GetComponent<PlayerMovement>().Restart();
-
-            hasDied = true;
-
-            Invoke("IsAlive", 1);
         }
-    }
-
-    private void IsAlive()
-    {
-        hasDied = false;
     }
 }
